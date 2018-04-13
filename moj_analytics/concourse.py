aafd29ec4606a3ffdf08340e8608c7edf633e6d0
Andy Driver <andy@pagezero.net>
2018-04-13 10:25:12 +0100

    Refactor Resource decorator to be simpler and less opinionated

--- moj_analytics/concourse.py
+++ moj_analytics/concourse.py
@@ -17,25 +17,8 @@ class Resource(object):
 
         self.kwargs = json.loads(sys.stdin.read())
 
-        if 'version' in self.kwargs:
-            self.kwargs['version'] = self.kwargs['version']['ref']
-
     def __call__(self):
         with redirect_stdout(sys.stderr):
             response = self.fn(*self.args, **self.kwargs)
-
-        if self.action in ('in', 'out'):
-            version, metadata = response
-            response = {
-                'version': { 'ref': version },
-            }
-            if metadata:
-                response['metadata'] = [
-                    {'name': name, 'value': value}
-                    for name, value in metadata.items()
-                ]
-
-        elif self.action == 'check':
-            response = [{ 'ref': str(v) } for v in response]
 
         print(json.dumps(response))
